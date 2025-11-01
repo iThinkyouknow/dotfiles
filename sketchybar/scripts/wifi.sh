dc7a00a595312e4e0d09451748facbbb7da9c16b
iThinkyouknow <woontlt.dev@gmail.com>
2025-11-01 01:53:18 -0400

    wifi

---
+++ sketchybar/scripts/wifi.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+get_icon() {
+    if echo "$ssid" | grep -qi "iphone"; then # lol what a hack
+    icon=􀉤
+  elif [[ $ssid != "" ]]; then
+    icon=􀙇
+  else
+    icon=􀙈
+  fi   
+}
+
+ssid=$(ipconfig getsummary en0 | awk -F' SSID : ' '/ SSID : / {print $2}')
+get_icon
+
+sketchybar --set $NAME label="$ssid" \
+                       icon=$icon
